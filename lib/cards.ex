34f7ad8e7f4d07d03b6fc7e3cbb820a318bf7080
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-30 16:00:35 -0400

    working through the tutorial

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,13 +1,34 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "two", "three"]
+   values = ["Ace", "two", "three", "four", "five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    #solution number 1
+    # cards = for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    # end
 
+    # List.flatten(cards)
 
+      #the better and shorter way
+      for suit <- suits, value <- values do
+        
+          "#{value} of #{suit}"
+        end
+    
 
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  # the question mark does not mean anything specific, it just notes to other developers that the method will return a true/false value
+  def contains?(deck, card) do
+    #The member function just checks to see if the element exists within the enumerable
+    Enum.member?(deck, card)
+    
   end
   
 end
